07dca0ecf5ad03ffda3cd37ff9ef249ccdadb095
architolk <msj@xs4all.nl>
2018-11-15 23:02:33 +0100

    Added soap testen for wsdl

---
+++ soap-testen/GetWsdl-Local.sh
@@ -0,0 +1,1 @@
+curl -v --header "Content-type: text/xml;charset=UTF-8" http://localhost:8080/DomainTableWS.svc?wsdl -o output/GetWsdl-RWS.xml
---
+++ soap-testen/GetWsdl-RWS.sh
@@ -0,0 +1,1 @@
+curl -v --header "Content-type: text/xml;charset=UTF-8" http://domeintabellen-idsw-ws.rws.nl/DomainTableWS.svc?wsdl -o output/GetWsdl-RWS.xml
