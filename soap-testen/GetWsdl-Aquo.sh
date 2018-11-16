67bf95f827140bf225388622d079704ae239b13b
architolk <msj@xs4all.nl>
2018-11-16 15:53:49 +0100

    Update get wsdl test

--- soap-testen/GetWsdl-Aquo.sh
+++ soap-testen/GetWsdl-Aquo.sh
@@ -1,1 +1,1 @@
-curl -v --header "Content-type: text/xml;charset=UTF-8" http://standaard.aquo.nl/DomainTableWS.svc/wsdl -o output/GetWsdl-Aquo.xml
+curl -v --header "Content-type: text/xml;charset=UTF-8" http://standaard.aquo.nl/DomainTableWS.svc?wsdl -o output/GetWsdl-Aquo.xml
--- soap-testen/GetWsdl-Local.sh
+++ soap-testen/GetWsdl-Local.sh
@@ -1,1 +1,1 @@
-curl -v --header "Content-type: text/xml;charset=UTF-8" http://localhost:8080/DomainTableWS.svc/wsdl -o output/GetWsdl-Local.xml
+curl -v --header "Content-type: text/xml;charset=UTF-8" http://localhost:8080/DomainTableWS.svc?wsdl -o output/GetWsdl-Local.xml
